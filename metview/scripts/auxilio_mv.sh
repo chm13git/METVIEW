ef6a60c3e95bd020eb94bd9875200b26aae4e5d2
CHM13 <flavia.pinheiro@marinha.mil.br>
2021-07-14 22:16:18 -0300

    Incluindo os dois AD para as duas máquinas de pós.

--- metview/scripts/auxilio_mv.sh
+++ metview/scripts/auxilio_mv.sh
@@ -5,14 +5,14 @@
 if [ $# -ne 1 ];then
 
         echo "Entre com a data e com o horario de referencia (00 ou 12)."
-        echo "Ex.: auxilio_mv.sh 00"
+        echo "Ex.: auxilio_novo_nebrasil.sh 00"
         exit
 
 fi
 
 DATA=`date +%Y%m%d`
 HH=$1
-#DATA=20210628
+#DATA=20210712
 
 echo Inicio: `date`
 echo
@@ -23,11 +23,11 @@ cd /home/operador/metview/neris/work
 
 metview -b /home/operador/metview/neris/nebrasil_emborg_cosmo.mv $DATA $HH
 metview -b /home/operador/metview/neris/nebrasil_opeanv_cosmo.mv $DATA $HH
-#metview -b nebrasil_emborg_cosmo.mv $DATA $HH
-#metview -b nebrasil_opeanv_cosmo.mv $DATA $HH
 
 echo "Transferindo figuras para o Supervisor (DPNT02B e DPNT02C)"
-scp auxilio_mv_nebrasil_*_${HH}_*.png supervisor@10.13.200.5:~/grads/gif/auxilio$HH/cosmomet/ww3met
-scp auxilio_mv_nebrasil_*_${HH}_*.png supervisor@10.13.200.8:~/grads/gif/auxilio$HH/cosmomet/ww3met
+scp auxilio_mv_nebrasil_emborg*_${HH}_*.png supervisor@10.13.200.5:~/grads/gif/auxilio$HH/cosmomet/ww3met
+scp auxilio_mv_nebrasil_opeanv*_${HH}_*.png supervisor@10.13.200.5:~/grads/gif/auxilio$HH/cosmomet/ww3met
+scp auxilio_mv_nebrasil_emborg*_${HH}_*.png supervisor@10.13.200.8:~/grads/gif/auxilio$HH/cosmomet/ww3met
+scp auxilio_mv_nebrasil_opeanv*_${HH}_*.png supervisor@10.13.200.8:~/grads/gif/auxilio$HH/cosmomet/ww3met
 
 echo Final: `date`
